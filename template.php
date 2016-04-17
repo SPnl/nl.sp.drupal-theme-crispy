af940b1747ffcb9bb16c2eaa9f5cc76a58edf15a
sdekker@sp.nl <vagrant@drupal-dev>
2016-04-17 14:54:16 +0000

    Cleanup

--- template.php
+++ template.php
@@ -4,12 +4,10 @@
  * Override or insert variables into the maintenance page template.
  */
 function crispy_preprocess_maintenance_page(&$vars) {
-  // While markup for normal pages is split into page.tpl.php and html.tpl.php,
-  // the markup for the maintenance page is all in the single
+  // Markup for the maintenance page is all in the single
   // maintenance-page.tpl.php template. So, to have what's done in
-  // seven_preprocess_html() also happen on the maintenance page, it has to be
-  // called here.
-  seven_preprocess_html($vars);
+  // seven_preprocess_html():
+  crispy_preprocess_html($vars);
 }
 
 /**
